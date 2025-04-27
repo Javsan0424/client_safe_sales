e7d8d8f27e65e1c330eed87d1348c817e31138b3
Javsan0424 <A01198909@tec.mx>
2025-04-27 00:26:46 -0600

    Updated game

--- src/app/Juego/page.tsx
+++ src/app/Juego/page.tsx
@@ -13,7 +13,7 @@ export default function Juego() {
             <main className="flex-1 p-10 overflow-auto">
                 <div className="text-5xl mb-4">Juego</div>
                 <iframe
-                    src="https://javsan0424.github.io/Game_test/"
+                    src="https://github.com/ManuelPerezF/CRM"
                     width="100%"
                     height="650px"
                     className="border rounded-xl"
