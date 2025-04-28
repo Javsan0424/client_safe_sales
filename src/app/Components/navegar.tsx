9d7dd9f9ff51191e36dd9a9344fcd66fac1d01f0
Javsan0424 <A01198909@tec.mx>
2025-04-28 11:16:48 -0600

    Image update

--- src/app/Components/navegar.tsx
+++ src/app/Components/navegar.tsx
@@ -5,14 +5,16 @@ export default function Menu() {
         <div className="bg-gray-300 w-50 h-screen shadow-lg shadow-black">
             <div className="mt-10 ml-8">
                 <ol className="space-y-18">
-                    <div><img src="public\SafeSales Logo.jpg" alt="Processed Image" style={{ mixBlendMode: 'multiply' }} /></div>
-                    <div><Link href={"/Page1"} className="text-blue-500 hover:underline">Tablero</Link></div>
-                    <div><Link href={"/Empresas"} className="text-blue-500 hover:underline">Empresas</Link></div>
-                    <div><Link href={"/Clientes"} className="text-blue-500 hover:underline">Clientes</Link></div>
-                    <div><Link href={"/Productos"} className="text-blue-500 hover:underline">Productos</Link></div>
-                    <div><Link href={"/Negociaciones"} className="text-blue-500 hover:underline">Negociaciones</Link></div>
-                    <div><Link href={"/Ventas"} className="text-blue-500 hover:underline">Ventas</Link></div>
-                    <div><Link href={"/Juego"} className="text-blue-500 hover:underline">Juego Interactivo</Link></div>
+                    <div>
+                        <img src="/SafeSales Logo.jpg" alt="Processed Image" style={{ mixBlendMode: 'multiply' }} />
+                    </div>
+                    <div><Link href="/Page1" className="text-blue-500 hover:underline">Tablero</Link></div>
+                    <div><Link href="/Empresas" className="text-blue-500 hover:underline">Empresas</Link></div>
+                    <div><Link href="/Clientes" className="text-blue-500 hover:underline">Clientes</Link></div>
+                    <div><Link href="/Productos" className="text-blue-500 hover:underline">Productos</Link></div>
+                    <div><Link href="/Negociaciones" className="text-blue-500 hover:underline">Negociaciones</Link></div>
+                    <div><Link href="/Ventas" className="text-blue-500 hover:underline">Ventas</Link></div>
+                    <div><Link href="/Juego" className="text-blue-500 hover:underline">Juego Interactivo</Link></div>
                 </ol>
             </div>
         </div>
