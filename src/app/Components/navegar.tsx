40f495a4b7f3f48c41f53b2a6eaa93dcffe98fae
Javsan0424 <A01198909@tec.mx>
2025-04-28 11:35:47 -0600

    Image updated

--- src/app/Components/navegar.tsx
+++ src/app/Components/navegar.tsx
@@ -3,10 +3,10 @@ import Link from "next/link";
 export default function Menu() {
     return (
         <div className="bg-gray-300 w-50 h-screen shadow-lg shadow-black">
-            <div className="mt-10 ml-8">
-                <ol className="space-y-18">
-                    <div className="flex justify-center mt-10 mr-8">
-                        <img src="/SafeSales Logo.jpg" alt="Processed Image" style={{ mixBlendMode: 'multiply',  width: '200', height: '100', objectFit: 'contain'}} />
+            <div className="ml-8">
+                <ol className="space-y-17">
+                    <div className="flex justify-center mr-16">
+                        <img src="/SafeSales Logo.jpg" alt="Processed Image" style={{ mixBlendMode: 'multiply', objectFit: 'contain'}} />
                     </div>
                     <div><Link href="/Page1" className="text-blue-500 hover:underline">Tablero</Link></div>
                     <div><Link href="/Empresas" className="text-blue-500 hover:underline">Empresas</Link></div>
